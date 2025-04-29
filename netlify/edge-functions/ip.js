a38cf76848813b1df20ad912234889b3a03553a6
Young Park <77756987+cf-ypark@users.noreply.github.com>
2025-04-29 10:48:51 -0700

    Update ip.js

--- netlify/edge-functions/ip.js
+++ netlify/edge-functions/ip.js
@@ -1,1 +1,10 @@
+const ipReturn = async (request, context) => {
+    return new Response(`IP: ${request.socket.remoteAddress}`);
+};
+
+export const config = {
+    path: '/ip',
+};
+
+export default ipReturn;
 
